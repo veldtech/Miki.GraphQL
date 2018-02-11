6ad91d33c1e7a1c0a9c44dc78844cd47e593150a
Veld <velddev@gmail.com>
2018-02-11 15:55:52 +0100

    Add: GraphQL client

---
+++ GraphQLClient.cs
@@ -0,0 +1,121 @@
+﻿using Miki.GraphQL.Internal;
+using Newtonsoft.Json;
+using Rest;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Miki.GraphQL
+{
+    public class GraphQLClient
+    {
+		RestClient restClient;
+
+		public GraphQLClient(string url)
+		{
+			restClient = new RestClient(url);
+		}
+
+		/// <summary>
+		/// Query the endpoint with a raw function and receive the json response, for parameters use $p0, $p1... to access your variables
+		/// </summary>
+		/// <param name="query">GraphQL query</param>
+		/// <param name="variables">Variables used in query</param>
+		/// <returns>Json response</returns>
+		public async Task<string> QueryAsync(string query, params object[] variables)
+			=> await InternalQueryAsync(CreateQueryJson(query, variables));
+		/// <summary>
+		/// Query the endpoint with a raw function.
+		/// </summary>
+		/// <param name="query">GraphQL query</param>
+		/// <param name="variables">Variables used in query</param>
+		/// <returns>Object of type T converted</returns>
+		public async Task<string> QueryAsync(string query, params KeyValuePair<string, object>[] variables)
+			=> await InternalQueryAsync(CreateQueryJson(query, variables));
+
+		/// <summary>
+		/// Query the endpoint with a raw function, for parameters use $p0, $p1... to access your variables
+		/// </summary>
+		/// <typeparam name="T">The output object serialized to</typeparam>
+		/// <param name="query">GraphQL query</param>
+		/// <param name="variables">Variables used in query</param>
+		/// <returns>Object of type T converted</returns>
+		public async Task<T> QueryAsync<T>(string query, params object[] variables)
+			=> await InternalQueryAsync<T>(CreateQueryJson(query, variables));
+		/// <summary>
+		/// Query the endpoint with a raw function
+		/// </summary>
+		/// <typeparam name="T">The output object serialized to</typeparam>
+		/// <param name="query">GraphQL query</param>
+		/// <param name="variables">Variables used in query</param>
+		/// <returns>Object of type T converted</returns>
+		public async Task<T> QueryAsync<T>(string query, params KeyValuePair<string, object>[] variables)
+			=> await InternalQueryAsync<T>(CreateQueryJson(query, variables));
+
+		/// <summary>
+		/// Query GraphQL string and receive json
+		/// </summary>
+		/// <param name="query">GraphQL query</param>
+		/// <returns>Json response</returns>
+		internal async Task<string> InternalQueryAsync(string query)
+			=> (await restClient.PostAsync<string>("", query)).Body;
+
+		/// <summary>
+		/// Query GraphQL string and receive serialized object
+		/// </summary>
+		/// <param name="query">GraphQL query</param>
+		/// <returns>Response of type T</returns>
+		internal async Task<T> InternalQueryAsync<T>(string query)
+		{
+			RestResponse<GraphQLQuery<T>> response = await restClient.PostAsync<GraphQLQuery<T>>("", query);
+			if (response.Success)
+			{
+				return response.Data.Data;
+			}
+			return default(T);
+		}
+
+		/// <summary>
+		/// Utility function to create queries for post messages
+		/// </summary>
+		/// <param name="query">base query</param>
+		/// <param name="variables">variables from query</param>
+		/// <returns>postable query</returns>
+		string CreateQueryJson(string query, params object[] variables)
+		{
+			Dictionary<string, object> allVariables = new Dictionary<string, object>();
+
+			for (int i = 0; i < variables.Length; i++)
+			{
+				allVariables.Add($"p{i}", variables[i]);
+			}
+
+			return CreateQueryJson(query, allVariables);
+		}
+		/// <summary>
+		/// Utility function to create queries for post messages
+		/// </summary>
+		/// <param name="query">base query</param>
+		/// <param name="variables">variables from query</param>
+		/// <returns>postable query</returns>
+		string CreateQueryJson(string query, params KeyValuePair<string, object>[] variables)
+		{
+			Dictionary<string, object> allVariables = new Dictionary<string, object>();
+			foreach (var v in variables)
+				allVariables.Add(v.Key, v.Value);
+
+			return CreateQueryJson(query, allVariables);
+		}
+		/// <summary>
+		/// Utility function to create queries for post messages
+		/// </summary>
+		/// <param name="query">base query</param>
+		/// <param name="variables">variables from query</param>
+		/// <returns>postable query</returns>
+		string CreateQueryJson(string query, Dictionary<string, object> variables)
+		{
+			string serializedVariables = JsonConvert.SerializeObject(variables);
+			return $"{{ \"query\": \"{query}\", \"variables\": {serializedVariables} }}";
+		}
+	}
+}
---
+++ Internal/GraphQLQuery.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Miki.GraphQL.Internal
+{
+	internal class GraphQLQuery<T>
+	{
+		[JsonProperty("data")]
+		internal T Data;
+	}
+}
